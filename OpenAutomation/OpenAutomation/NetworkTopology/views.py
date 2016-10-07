56d9037c522433647945bc61c28cab802ba0a035
Steven-N <steven44121@gmail.com>
2016-10-07 01:29:46 -0400

    Sending Nodes and Edges back at the same time.

--- OpenAutomation/OpenAutomation/NetworkTopology/views.py
+++ OpenAutomation/OpenAutomation/NetworkTopology/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
+import ast
+from json import loads
 
 
 def index(request):
@@ -10,6 +12,8 @@ def index(request):
 @csrf_exempt
 def returnjson(request):
     if request.is_ajax():
-        request_data = request.POST
-        print("Raw Data: " + str(request_data))
+        request_data = request.body
+        print(str(request_data))
+        #data = ast.literal_eval(request_data.decode('utf-8'))
+        #print(data[0].keys())
         return HttpResponse("OK")
